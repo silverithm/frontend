248f14786ebea496eb64e01f41177676037a39e8
jun <ggprgrkjh@naver.com>
2024-03-20 23:43:17 +0900

    feat(Header): add signin api with jwtbox

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,40 @@ import { styled } from "styled-components";
 
 function Header() {
   const [name, setName] = useState("");
-  const [id, setId] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [jwtExpiredTime, setJwtExpiredTime] = useState(0);
   const [jwt, setJwt] = useState("");
 
-  const handleSignin = () => {
-    // 로그인 처리 로직
+  const handleSignin = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+      role: "ROLE_ADMIN",
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    const loginResult = await fetch(
+      "http://localhost:8080/api/v1/signin",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => console.error(error));
+
+    await setJwt(loginResult.substring(37, 197));
   };
 
   const handleSignup = () => {
@@ -24,39 +51,45 @@ function Header() {
 
   return (
     <HeaderDiv>
-      <input
-        type="text"
-        placeholder="name"
-        value={name}
-        onChange={(e) => setId(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="ID"
-        value={id}
-        onChange={(e) => setId(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignin}>로그인</button>
-      <button onClick={handleLogout}>로그아웃</button>
-      <button onClick={handleSignup}>회원가입</button>
-      <input
-        type="jwt-expired-time"
-        placeholder="JwtExpiredTime"
-        value={jwtExpiredTime}
-        onChange={(e) => setJwtExpiredTime(e.target.value)}
-      />
-      <input
-        type="jwt"
-        placeholder="Jwt"
-        value={jwt}
-        onChange={(e) => setJwt(e.target.value)}
-      />
+      <div>
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="ID"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleSignin}>로그인</button>
+        <button onClick={handleLogout}>로그아웃</button>
+        <button onClick={handleSignup}>회원가입</button>
+        <input
+          type="jwt-expired-time"
+          placeholder="JwtExpiredTime"
+          value={jwtExpiredTime}
+          onChange={(e) => setJwtExpiredTime(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <input
+          type="jwt"
+          placeholder="Jwt"
+          value={jwt}
+          onChange={(e) => setJwt(e.target.value)}
+          style={{ width: "1220px" }}
+        />
+      </div>
     </HeaderDiv>
   );
 }
@@ -67,6 +100,7 @@ const HeaderDiv = styled.div`
   align-content: center;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   display: flex;
 
   background: papayawhip;
