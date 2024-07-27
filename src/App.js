32bf7e02464185d29b3be95b4f2c8f4e66b33cf8
jun <ggprgrkjh@naver.com>
2024-07-28 01:08:29 +0900

    feat: 배치 고정 체크 박스 제거

--- src/App.js
+++ src/App.js
@@ -584,19 +584,6 @@ function App() {
                 <table className="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400 table-auto">
                   <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                      <th scope="col" className="p-4">
-                        <div className="flex items-center">
-                          <input
-                            id="fixCheckbox-all"
-                            type="checkbox"
-                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                            onChange={handleFixCheckboxAll}
-                          />
-                          <label htmlFor="checkbox-all" className="sr-only">
-                            checkbox
-                          </label>
-                        </div>
-                      </th>
                       <th scope="col" className="px-6 py-3">
                         이름
                       </th>
@@ -614,21 +601,6 @@ function App() {
                           selectedRows.has(index) ? "bg-blue-200" : ""
                         } hover:bg-blue-100`}
                       >
-                        <td className="w-4 p-4">
-                          <div className="flex items-center">
-                            <input
-                              id={`fixCheckbox-table-${employee.id}`}
-                              type="checkbox"
-                              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                            />
-                            <label
-                              htmlFor={`checkbox-table-${employee.id}`}
-                              className="sr-only"
-                            >
-                              checkbox
-                            </label>
-                          </div>
-                        </td>
                         <td className="px-6 py-4">{employee.name}</td>
                         <td className="px-6 py-4">
                           {Array.from(
