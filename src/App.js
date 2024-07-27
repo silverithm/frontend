79e07f53092e6bcc4283700149a4076b55eb8fc8
jun <ggprgrkjh@naver.com>
2024-07-27 19:44:18 +0900

    feat: fetchEldersAndEmployee 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import ReportProblemIcon from "@mui/icons-material/ReportProblem";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import Form from "react-bootstrap/Form";
+import { toast, ToastContainer } from "react-toastify";
+import config from "../config";
+
 const initialRows = [
   {
     id: 1,
@@ -111,6 +115,73 @@ function App() {
   const [isEmployeeCollapsed, setIsEmployeeCollapsed] = useState(true);
   const [isElderCollapsed, setIsElderCollapsed] = useState(true);
   const [maxDisaptchStatus, setMaxDispatchStatus] = useState("under");
+
+  const [elders, setElders] = useState([]);
+  const [employees, setEmployees] = useState([]);
+
+  const fetchEmployeesAndElders = async () => {
+    if (jwt === "") {
+      toast(
+        "불러오기를 하려면 로그인이 필요합니다. 먼저 로그인을 시도해 주세요."
+      );
+      return;
+    }
+    await fetchEmployees();
+    await fetchElders();
+  };
+
+  const fetchEmployees = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + jwt);
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    const response = await fetch(
+      `${config.apiUrl}/employees/` + userId,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => console.error(error));
+
+    await setEmployees(response); // 상태 업데이트
+  };
+  const fetchElders = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + jwt);
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    const response = await fetch(
+      `${config.apiUrl}/elders/` + userId,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => console.error(error));
+
+    await setElders(response); // 상태 업데이트
+  };
+
+  const handleSelect = (employeeId, elderId, sequence) => {
+    const selectedAssignment = {
+      employee_id: employeeId === "없음" ? "없음" : Number(employeeId),
+      elderly_id: elderId,
+      sequence: sequence,
+    };
+
+    // onSelectAssignment(selectedAssignment);
+  };
 
   const handleEdit = (id) => {
     // 수정 로직 추가
@@ -430,6 +501,118 @@ function App() {
                 </table>
               </div>
             </div>
+
+            <div className="h-10"></div>
+
+            <div>
+              <div className="flex flex-row items-center justify-between mb-4">
+                <div className="flex flex-row items-center">
+                  <text className="text-lg font-bold">배치 고정</text>
+                  <div className="w-6"></div>
+                  <button className="text-sm">현재 고정 인원 5명 +</button>
+                </div>
+
+                <div className="flex flex-row mr-1">
+                  <button
+                    onClick={() => setIsElderCollapsed(!isElderCollapsed)}
+                    className="text-sm bg-sky-950 text-white w-20 h-8 rounded "
+                  >
+                    {isElderCollapsed ? "늘리기" : "접기"}
+                  </button>
+                  <div className="w-4"></div>
+
+                  <button className="text-sm bg-sky-950 text-white w-20 h-8 rounded ">
+                    어르신 추가
+                  </button>
+                </div>
+              </div>
+
+              <div
+                className={`relative overflow-x-auto shadow-md ${
+                  isElderCollapsed ? "h-80 overflow-y-scroll" : ""
+                }`}
+              >
+                <table className="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400 table-auto">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="p-4">
+                        <div className="flex items-center">
+                          <input
+                            id="elderCheckbox-all"
+                            type="checkbox"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                            onChange={handleElderCheckboxAll}
+                          />
+                          <label htmlFor="checkbox-all" className="sr-only">
+                            checkbox
+                          </label>
+                        </div>
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        이름
+                      </th>
+
+                      <th scope="col" className="px-6 py-3">
+                        고정
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {employees.map((employee, index) => (
+                      <tr
+                        key={index}
+                        className={`${
+                          selectedRows.has(index) ? "bg-blue-200" : ""
+                        } hover:bg-blue-100`}
+                      >
+                        <td className="w-4 p-4">
+                          <div className="flex items-center">
+                            <input
+                              id={`elderCheckbox-table-${index}`}
+                              type="checkbox"
+                              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                            />
+                            <label
+                              htmlFor={`checkbox-table-${index}`}
+                              className="sr-only"
+                            >
+                              checkbox
+                            </label>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4">{employee.name}</td>
+                        <td className="px-6 py-4">
+                          {Array.from(
+                            { length: employee.maximumCapacity },
+                            (_, index) => (
+                              <Form.Select
+                                key={index}
+                                style={{ textAlign: "center", width: "100px" }}
+                                onChange={(e) =>
+                                  handleSelect(
+                                    employee.id,
+                                    e.target.value,
+                                    index + 1
+                                  )
+                                }
+                              >
+                                <option value="없음">없음</option>
+                                {elders.map((elder, idx) => (
+                                  <option key={idx} value={elder.id}>
+                                    {elder.name}
+                                  </option>
+                                ))}
+                              </Form.Select>
+                            )
+                          )}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="h-10"></div>
           </div>
         ) : (
           <div>이전 배치 보기</div>
